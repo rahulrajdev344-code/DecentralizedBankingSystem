e3ed2363090e1b3250018bf005ebebcb24dbc190
rahulrajdev344-code <rahulraj.dev344@gmail.com>
2026-01-31 22:42:00 +0530

    Deploy contracts to Sepolia testnet

--- backend/web3conn.js
+++ backend/web3conn.js
@@ -2,10 +2,13 @@ const Web3 = require("web3");
 const contract = require("truffle-contract");
 const artifacts = require("./build/Functions.json");
 
+// Use Alchemy Sepolia RPC for production
+const ALCHEMY_URL = process.env.ALCHEMY_URL || "https://eth-sepolia.g.alchemy.com/v2/PypPPIrep6rmbCH7w5-FP";
+
 if (typeof web3 !== "undefined") {
 	var web3 = new Web3(web3.currentProvider);
 } else {
-	var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+	var web3 = new Web3(new Web3.providers.HttpProvider(ALCHEMY_URL));
 }
 const LMS = contract(artifacts);
 LMS.setProvider(web3.currentProvider);
