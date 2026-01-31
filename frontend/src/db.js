060faaf86cff88fc9bf887f28c8051f27f7b69bc
rahulrajdev344-code <rahulraj.dev344@gmail.com>
2026-01-31 21:32:24 +0530

    Fix: Use env vars for Firebase config

--- frontend/src/db.js
+++ frontend/src/db.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 const firebaseConfig = {
-	apiKey: "AIzaSyDNv-liOygrY17bqUNObJ9HjH7RKCuHkJA",
-	authDomain: "decentralized-banking-system.firebaseapp.com",
-	projectId: "decentralized-banking-system",
-	storageBucket: "decentralized-banking-system.appspot.com",
-	messagingSenderId: "258229683916",
-	appId: "1:258229683916:web:f85a407db0d82c2839e469",
-	measurementId: "G-G4N400HRYC",
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
